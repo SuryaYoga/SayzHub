29aea21d7831160dc1519564766ffea25dffb816
SuryaYoga <86194154+SuryaYoga@users.noreply.github.com>
2026-02-26 19:44:42 +0800

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,35 +1,32 @@
--- [[ CONFIGURATION ]] --
 getgenv().SayzConfig = {
-    UserName = "SuryaYoga", -- GANTI INI
-    Repo = "SayzHub",   -- GANTI INI
+    UserName = "SuryaYoga",
+    Repo = "SayzHub",
     Branch = "main"
 }
 
 local function GetRaw(path)
-    return string.format("https://raw.githubusercontent.com/%s/%s/%s/%s", 
+    return string.format("https://raw.githubusercontent.com/%s/%s/%s/%s",
         getgenv().SayzConfig.UserName, getgenv().SayzConfig.Repo, getgenv().SayzConfig.Branch, path)
 end
 
--- [[ LOAD UI LIBRARY ]] --
-local SayzUI = loadstring(game:HttpGet("https://pastebin.com/raw/hys38rNm"))()
+-- load library dari repo (disarankan)
+local SayzUI = loadstring(game:HttpGet(GetRaw("Library/SayzUI.lua")))()
 
 local Window = SayzUI:CreateWindow({
     Title = "SayzUI v1",
     Subtitle = "BlueWhite Edition",
     Theme = "BlueWhite",
-    Keybind = Enum.KeyCode.K,
+
+    ToggleKeybind = Enum.KeyCode.K,
     ShowWelcomeToast = true,
-    Loading = {
-        Enabled = true,
-        Text = "Loading SayzUI v1...",
-        Duration = 1.5
-    }
+
+    ShowLoading = true,
+    LoadingText = "Loading SayzUI v1...",
+    LoadingDuration = 1.5
 })
 
--- [[ GLOBAL SETTINGS ]] --
--- Variabel yang sering diakses antar tab ditaruh di getgenv()
 getgenv().PnBSettings = {
-    TargetID = 0, DelayScale = 1, ActualDelay = 0.12, PlaceDelay = 0.05, 
+    TargetID = 0, DelayScale = 1, ActualDelay = 0.12, PlaceDelay = 0.05,
     Posisi = "1 Atas", Jumlah = 1, BreakMode = "Mode 1 (Fokus)",
     Place = false, Break = false, Master = false, Scanning = false,
     SelectedTiles = {}, OriginGrid = nil, LockPosition = false
@@ -40,11 +37,8 @@ getgenv().CheatSettings = {
     FlySpeed = 50
 }
 
--- [[ LOAD MODULES ]] --
--- Memanggil Tab-Tab Utama
 loadstring(game:HttpGet(GetRaw("Modules/Beranda.lua")))()(Window)
 loadstring(game:HttpGet(GetRaw("Modules/Automation.lua")))()(Window)
 loadstring(game:HttpGet(GetRaw("Modules/Miscs.lua")))()(Window)
-
 
 Window:Notify("SayzUI Modular Berhasil Dimuat!", 3, "ok")
